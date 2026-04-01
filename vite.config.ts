2f485c12e9b254dee29a1b4344706b198c4f5367
Jac LL <210639514+J-Yhwh@users.noreply.github.com>
2026-04-01 20:31:43 +0800

    Remove external configuration for @vueuse/core

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      // Tell Rollup to treat @vueuse/core as external (don't bundle it)
-      external: ['@vueuse/core'],
-    },
-  },
 })
