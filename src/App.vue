2be2c3f567e8f40da5718b2a399e871094afebb0
Jac LL <210639514+J-Yhwh@users.noreply.github.com>
2026-05-14 09:46:47 +0800

    Update copyright notice in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Your Name All Rights Reserved</p>
+<p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Jac LL. All Rights Reserved</p>
 
 
 <template>
